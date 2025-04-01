498d2849b7b0f76712e7283378a0f7aa76567933
Mohamed <douzimohamed.pro@gmail.com>
2025-04-01 14:43:32 +0200

    docmoha

--- Question 8/mohamed.py
+++ Question 8/mohamed.py
@@ -2,12 +2,21 @@ import pandas as pd
 
 fichier_source = "data/Match.csv"
 df = pd.read_csv(fichier_source)
-# teams = pd.read_csv("data/Team.csv")[["team_api_id", "team_long_name"]]
+teams = pd.read_csv("data/Team.csv")[["team_api_id", "team_long_name"]]
+
+
 home_team = df["home_team_api_id"].tolist()
 away_team = df["away_team_api_id"].tolist()
 
 home_goal = df["home_team_goal"].tolist()
 away_goal = df["away_team_goal"].tolist()
+
+def occurence(L,a):
+    c = 0
+    for i in range(len(L)):
+        if a == L[i]:
+            c+=1
+    return c
 
 d = {}
 for i in range(len(home_team)):
@@ -19,9 +28,9 @@ for i in range(len(away_team)):
 for e in d:
     for i in range(len(home_team)):
         if e == home_team[i]:
-            d[e] += away_goal[i]
+            d[e] += away_goal[i]/occurence(away_team + home_team, home_team[i])
         elif e == away_team[i]:
-            d[e] += home_goal[i]
+            d[e] += home_goal[i]/occurence(away_team + home_team, home_team[i])
 
 print(d)
 
@@ -38,20 +47,14 @@ def minimum(dico):
 
 print(minimum(d))
 
+id_teams = teams["team_api_id"].tolist()
+name_team = teams["team_long_name"].tolist()
 
-"""
+d2={}
+for i in range(len(id_teams)):
+    d2[id_teams[i]] = name_team[i]
 
-buts_encaissés_domicile =
-df.groupby("home_team_api_id")["away_team_goal"].sum() buts_encaissés_extérieur
-= df.groupby("away_team_api_id")["home_team_goal"].sum()
+'''
+nom_equipe = d2[8121]
 
-buts_totaux_encaissés = buts_encaissés_domicile.add(buts_encaissés_extérieur,
-fill_value=0)
-
-equipe_moins_buts = buts_totaux_encaissés.idxmin()
-
-nom_equipe = teams.loc[teams["team_api_id"] == equipe_moins_buts,
-"team_long_name"].values[0]]
-
-print(f"L'équipe qui a encaissé le moins de buts est : {nom_equipe}") '
-"""
+print(f"L'équipe qui a encaissé le moins de buts est : {nom_equipe}")'''
