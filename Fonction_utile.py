1e60fc84c1fe9322714184871d32e80c0f638f9a
AGasowski <>
2025-04-24 12:05:20 +0200

    int

--- Fonction_utile.py
+++ Fonction_utile.py
@@ -14,40 +14,28 @@ def transforme(X):
     Retourne:
     ---------
     pd.DataFrame
-        Un DataFrame contenant les données extraites du XML, avec les attributs des éléments "value"
-        comme colonnes et les sous-éléments de "stats" préfixés par "stats_".
+        Un DataFrame contenant les données extraites du XML, avec les attributs
+        des éléments "value" comme colonnes et les sous-éléments de "stats"
+        préfixés par "stats_".
 
     Exemples:
     ---------
-    ```python
-    xml_data = \"\"\"<root>
+    ```python xml_data = \"\"\"<root>
         <value>
-            <name>John</name>
-            <age>30</age>
-            <stats>
-                <height>180</height>
-                <weight>75</weight>
+            <name>John</name> <age>30</age> <stats>
+                <height>180</height> <weight>75</weight>
             </stats>
-        </value>
-        <value>
-            <name>Jane</name>
-            <age>28</age>
-            <stats>
-                <height>165</height>
-                <weight>60</weight>
+        </value> <value>
+            <name>Jane</name> <age>28</age> <stats>
+                <height>165</height> <weight>60</weight>
             </stats>
         </value>
     </root>\"\"\"
 
-    df = transforme(xml_data)
-    print(df)
-    ```
-    Résultat :
-    ```
+    df = transforme(xml_data) print(df) ``` Résultat : ```
        name age  stats_height  stats_weight
-    0  John  30          180            75
-    1  Jane  28          165            60
-    ```
+    0  John  30          180            75 1  Jane  28          165
+    60 ```
     """
     root = ET.fromstring(X)
     data = []
