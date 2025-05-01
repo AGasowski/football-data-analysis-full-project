0974c3ec4b7ef506f84245a7f178486f15204dc5
elhadji <freshafro48@gmail.com>
2025-05-01 22:49:30 +0200

    doc el hajdji

--- python/Q11_pire_ratio.panda.py
+++ python/Q11_pire_ratio.panda.py
@@ -1,7 +1,7 @@
 from fonctions_communes import (
     lire_csv,
     select_all,
-    select_colonnes,name_team_dic,
+    select_colonnes,name_team_dic,transforme
     )  
 from fonction_commune_elhadji import (element_min_colonne,data_to_dict,moyenne_par_colonne,cle_minimale)  
 match = lire_csv("data/Match.csv")
@@ -17,6 +17,9 @@ match["ratio"] = match["but"]/match["tir_cadre"]
 d=moyenne_par_colonne(match,"home_team_api_id","ratio")
 team_names=data_to_dict(team,"team_api_id","team_long_name")
 print(name_team_dic(team_names,cle_minimale(d)))  
+
+
+
 
 
 
